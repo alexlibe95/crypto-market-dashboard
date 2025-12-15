1213e2890065d063ed1efb8aa3eb6274cee88211
alexlibe95 <alexlibe95@gmail.com>
2025-12-15 19:03:19 +0200

    search ui improvements

--- src/app/features/crypto/components/crypto-listing-view/crypto-search/crypto-search.component.ts
+++ src/app/features/crypto/components/crypto-listing-view/crypto-search/crypto-search.component.ts
@@ -1,22 +1,36 @@
-import { Component, ChangeDetectionStrategy, inject } from '@angular/core';
-import { Store } from '@ngrx/store';
+import {
+    Component,
+    ChangeDetectionStrategy,
+    inject
+  } from '@angular/core';
+  import { Store } from '@ngrx/store';
+  import { LucideAngularModule, XIcon, SearchIcon } from 'lucide-angular';
 
 import * as CryptoActions from '../../../store/crypto.actions';
-
-@Component({
-  selector: 'app-crypto-search',
-  templateUrl: './crypto-search.component.html',
-  styleUrl: './crypto-search.component.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush,
-})
-export class CryptoSearchComponent {
-  private store = inject(Store);
-
-  update(value: string): void {
-    this.store.dispatch(CryptoActions.updateSearch({ search: value }));
+import { selectSearch } from '../../../store/crypto.selectors';
+  
+  @Component({
+    selector: 'app-crypto-search',
+    imports: [LucideAngularModule],
+    templateUrl: './crypto-search.component.html',
+    styleUrl: './crypto-search.component.scss',
+    changeDetection: ChangeDetectionStrategy.OnPush
+  })
+  export class CryptoSearchComponent {
+    private store = inject(Store);
+  
+    readonly searchValue = this.store.selectSignal(selectSearch);
+    readonly xIcon = XIcon;
+    readonly searchIcon = SearchIcon;
+  
+    update(value: string): void {
+      this.store.dispatch(
+        CryptoActions.updateSearch({ search: value })
+      );
+    }
+  
+    clear(): void {
+      this.store.dispatch(CryptoActions.clearSearch());
+    }
   }
-
-  clear(): void {
-    this.store.dispatch(CryptoActions.clearSearch());
-  }
-}
+  
